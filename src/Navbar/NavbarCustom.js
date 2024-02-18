c4e2a2ba4276405bfde073823951cc39fa54bef0
ROSDAD <dadlani.r@northeastern.edu>
2024-02-18 10:42:22 -0500

    deploy

--- src/Navbar/NavbarCustom.js
+++ src/Navbar/NavbarCustom.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Container, Form, Button, Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Navigate, useNavigate } from 'react-router-dom';
 function NavbarCustom() {
+    const navigate = useNavigate();
     const handleLogout = () => {
+        navigate('/login')
         // Here you would handle the actual logout process, like clearing the user session
         alert('Logging out...');
       };
@@ -25,9 +28,9 @@ function NavbarCustom() {
             <LinkContainer to="/dashboard">
                 <Nav.Link >Dashboard</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/">
-                <Nav.Link >About Us</Nav.Link>
-            </LinkContainer>
+            {/* <LinkContainer to="/"> */}
+                {/* <Nav.Link >About Us</Nav.Link> */}
+            {/* </LinkContainer> */}
             {/* Add more navigation links as needed */}
           </Nav>
           <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
